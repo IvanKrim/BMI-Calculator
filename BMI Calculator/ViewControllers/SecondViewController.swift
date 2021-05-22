3e95a5e59a15e7492b3fad92a25c65a8a4145ba2
JohnKrim <reamonn96@gmail.com>
2021-05-22 15:17:15 +0300

    MVC pattern added

---
+++ BMI Calculator/ViewControllers/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Kalabishka Ivan on 22.05.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
