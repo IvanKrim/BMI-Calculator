b4e0d7140f7918cdfb60e1f3535c7af5d7bb37e7
JohnKrim <reamonn96@gmail.com>
2021-05-22 16:01:07 +0300

    firstScreen added

--- BMI Calculator/ViewControllers/FirstViewController.swift
+++ BMI Calculator/ViewControllers/FirstViewController.swift
@@ -9,13 +9,26 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
+    
+    @IBOutlet weak var heightValueLabel: UILabel!
+    @IBOutlet weak var weightValueLabel: UILabel!
+    
+    @IBOutlet var heightValue: UISlider!
+    @IBOutlet var weightValue: UISlider!
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightValueLabel.text = String(format: "%.2f", sender.value) + "m"
     }
     
-
- 
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightValueLabel.text = String(format: "%.0f", sender.value) + "Kg"
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        let heightValue = heightValue.value
+        let weightValue = weightValue.value
+        
+        let bmiResult = weightValue / pow(heightValue, 2)
+    }
+    
 }
